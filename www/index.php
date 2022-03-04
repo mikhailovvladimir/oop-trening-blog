b3511f5be2e79302932a2652b4d44889c352a49a
mikhailovvladimir96 <1602211@gmail.com>
2022-03-04 22:51:04 +0300

    added htaccess

--- www/index.php
+++ www/index.php
@@ -4,10 +4,4 @@ spl_autoload_register(function (string $className) {
     require_once __DIR__ . '/../src/' . str_replace('\\', '/', $className) . '.php';
 });
 
-$controller = new \MyProject\Controllers\MainController();
-
-if (!empty($_GET['name'])) {
-    $controller->sayHello($_GET['name']);
-} else {
-    $controller->main();
-}
+var_dump($_GET['route']);
