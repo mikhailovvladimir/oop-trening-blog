e6c2011920c2b1146847beb3e3e0234dbdac4856
mikhailovvladimir96 <1602211@gmail.com>
2022-03-21 22:35:49 +0300

    added composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '341e8f581d1f8e22aef22577ad267c7af0cae9c2',
+        'name' => 'mikhailov/blog.loc',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'erusev/parsedown' => array(
+            'pretty_version' => '1.7.4',
+            'version' => '1.7.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../erusev/parsedown',
+            'aliases' => array(),
+            'reference' => 'cb17b6477dfff935958ba01325f2e8a2bfa6dab3',
+            'dev_requirement' => false,
+        ),
+        'mikhailov/blog.loc' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '341e8f581d1f8e22aef22577ad267c7af0cae9c2',
+            'dev_requirement' => false,
+        ),
+    ),
+);
